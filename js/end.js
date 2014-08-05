92bbd68a8e6c9ac7f1bcc27d569f620f79fecee3
Justin <zertukis@gmail.com>
2014-08-05 01:15:25 -0500

    temporary game over screen added

--- js/end.js
+++ js/end.js
@@ -1,6 +1,11 @@
 var end_state = {
 	create: function() {
+		var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+			space_key.onDown.add(this.start, this);
 
+		var style = {font: "20px Arial", fill: "white"};
+
+		var text = this.game.add.text(150, 150, 'Press space to play Again', style);
 
 	},
 
